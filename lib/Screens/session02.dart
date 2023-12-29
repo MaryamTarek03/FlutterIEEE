fe44a2863e9dfea1dc80603bdbff1c9e366ad651
MaryamTarek03 <mariamtarek2233@gmail.com>
2023-12-29 10:53:48 +0200

    Task02

--- lib/Screens/session02.dart
+++ lib/Screens/session02.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class S2App01 extends StatelessWidget {
-  const S2App01({Key? key}) : super(key: key);
+  const S2App01({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,8 @@ class S2App01 extends StatelessWidget {
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.notifications,
+            child: Icon(
+              Icons.notifications,
               color: Colors.orange.shade500,
               size: 35,
             ),
@@ -20,51 +21,73 @@ class S2App01 extends StatelessWidget {
         ],
         title: const Padding(
           padding: EdgeInsets.all(10.0),
-          child: Text("NewsApp",
+          child: Text(
+            "NewsApp",
             style: TextStyle(
-                fontSize: 40,
-                color: Colors.black,
+              fontSize: 40,
+              color: Colors.black,
               fontWeight: FontWeight.bold,
             ),
           ),
         ),
       ),
-      body: SizedBox(
+      body: const SizedBox(
         width: double.infinity,
         child: Padding(
-          padding:  const EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
           child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
+            physics: BouncingScrollPhysics(),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text("Breaking News"
-                ,style: TextStyle(
+                Text(
+                  "Breaking News",
+                  style: TextStyle(
                     fontSize: 50,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(height: 20,),
+                SizedBox(
+                  height: 20,
+                ),
                 Center(
                   child: SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
-                    physics: const BouncingScrollPhysics(),
+                    physics: BouncingScrollPhysics(),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        MyImage(pic: "assets/Pictures/camping.jpg",text: "The weather is nice for camping today",),
-                        SizedBox(width: 10,),
-                        MyImage(pic: "assets/Pictures/SquareSquare.jpg",text: "A square in a square",),
-                        SizedBox(width: 10,),
-                        MyImage(pic: "assets/Pictures/Neeko.jpg",text: "Happy Halloween",),
-                        SizedBox(width: 10,),
+                      children: [
+                        MyImage(
+                          pic: "assets/Pictures/camping.jpg",
+                          text: "The weather is nice for camping today",
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        MyImage(
+                          pic: "assets/Pictures/SquareSquare.jpg",
+                          text: "A square in a square",
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        MyImage(
+                          pic: "assets/Pictures/Neeko.jpg",
+                          text: "Happy Halloween",
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 20,),
-                const Text("Recent News"
-                  ,style: TextStyle(
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Recent News",
+                  style: TextStyle(
                     fontSize: 50,
                     fontWeight: FontWeight.bold,
                   ),
@@ -72,16 +95,39 @@ class S2App01 extends StatelessWidget {
                 Center(
                   widthFactor: double.infinity,
                   child: Column(
-                    children: const [
-                      SizedBox(height: 20,),
-                      Item(pic: "assets/Pictures/Champie/Gwen-Champie-Icon.jpeg",text: "Gwen",),
-                      SizedBox(height: 10,),
-                      Item(pic: "assets/Pictures/Champie/Ivern-Champie-Icon.jpeg",text: "Ivern",),
-                      SizedBox(height: 10,),
-                      Item(pic: "assets/Pictures/Champie/KaiSa-Champie-Icon.jpeg",text: "Kai'Sa",),
-                      SizedBox(height: 10,),
-                      Item(pic: "assets/Pictures/Champie/Updated-Vex-Champie-Icon.jpeg",text: "Vex",),
-                      SizedBox(width: 10,),
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Item(
+                        pic: "assets/Pictures/Champie/Gwen-Champie-Icon.jpeg",
+                        text: "Gwen",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Item(
+                        pic: "assets/Pictures/Champie/Ivern-Champie-Icon.jpeg",
+                        text: "Ivern",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Item(
+                        pic: "assets/Pictures/Champie/KaiSa-Champie-Icon.jpeg",
+                        text: "Kai'Sa",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Item(
+                        pic:
+                            "assets/Pictures/Champie/Updated-Vex-Champie-Icon.jpeg",
+                        text: "Vex",
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
                     ],
                   ),
                 ),
@@ -98,24 +144,40 @@ class S2App01 extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.black,
           borderRadius: BorderRadius.circular(20),
-
         ),
-        child: Row(
+        child: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Expanded(child: Icon(Icons.home,color: Colors.white,size: 40,)),
-            Expanded(child: Icon(Icons.video_collection_outlined,color: Colors.white,size: 40,)),
-            Expanded(child: Icon(Icons.save,color: Colors.white,size: 40,)),
+          children: [
+            Expanded(
+                child: Icon(
+              Icons.home,
+              color: Colors.white,
+              size: 40,
+            )),
+            Expanded(
+                child: Icon(
+              Icons.video_collection_outlined,
+              color: Colors.white,
+              size: 40,
+            )),
+            Expanded(
+                child: Icon(
+              Icons.save,
+              color: Colors.white,
+              size: 40,
+            )),
           ],
         ),
       ),
     );
   }
 }
+
 class Item extends StatelessWidget {
-  const Item({Key? key,required this.pic, required this.text}) : super(key: key);
-  final String pic,text;
+  const Item({super.key, required this.pic, required this.text});
+  final String pic, text;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -132,16 +194,22 @@ class Item extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(15),
               child: SizedBox(
-                  width:150,
+                  width: 150,
                   height: 150,
-                  child: Image(image: AssetImage(pic,),fit: BoxFit.cover)
-              ),
+                  child: Image(
+                      image: AssetImage(
+                        pic,
+                      ),
+                      fit: BoxFit.cover)),
             ),
-            const SizedBox(width: 30,),
+            const SizedBox(
+              width: 30,
+            ),
             Expanded(
               // alignment: Alignment.centerLeft,
-              child: Text(text
-                  ,style: const TextStyle(
+              child: Text(
+                text,
+                style: const TextStyle(
                   // overflow: TextOverflow.ellipsis,
                   fontSize: 40,
                   color: Colors.white,
@@ -155,14 +223,14 @@ class Item extends StatelessWidget {
     );
   }
 }
+
 class MyImage extends StatelessWidget {
-  const MyImage({ Key? key, required this.pic, required this.text}) : super(key: key);
-  final String pic,text;
+  const MyImage({super.key, required this.pic, required this.text});
+  final String pic, text;
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
+    return Stack(alignment: Alignment.bottomCenter, children: [
       Container(
         clipBehavior: Clip.antiAliasWithSaveLayer,
         width: 450,
@@ -171,23 +239,17 @@ class MyImage extends StatelessWidget {
           color: Colors.black,
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Image(image: AssetImage(pic),fit: BoxFit.cover),
+        child: Image(image: AssetImage(pic), fit: BoxFit.cover)
       ),
-        Padding(
+      Padding(
           padding: const EdgeInsets.all(8.0),
           child: SizedBox(
-            width: 400,
-            child: Text(
-              text,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      ]
-    );
+              width: 400,
+              child: Text(text,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold))))
+    ]);
   }
 }
